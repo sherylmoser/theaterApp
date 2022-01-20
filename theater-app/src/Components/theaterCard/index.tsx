aa877db0172ab2b4013ee51586b56921414354d5
sherylmoser <sherylrmoser@gmail.com>
2022-01-20 15:05:08 -0700

    firebase initialized

--- theater-app/src/Components/theaterCard/index.tsx
+++ theater-app/src/Components/theaterCard/index.tsx
@@ -1,3 +1,3 @@
-export function SavedTheaters() {
+export function TheaterCard() {
 
 }
---
+++ theater-app/src/firebase/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyARFqRZo5aiNDXPi7anHBOzOYTvRT2Qo_o",
+    authDomain: "onstagetheaterapp.firebaseapp.com",
+    projectId: "onstagetheaterapp",
+    storageBucket: "onstagetheaterapp.appspot.com",
+    messagingSenderId: "640759180361",
+    appId: "1:640759180361:web:5827ab51fe8ac81ecaa398"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
