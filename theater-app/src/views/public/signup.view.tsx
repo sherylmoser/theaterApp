e10548f513fb67d812aa8b1b8a9ba709dcf541fd
sherylmoser <sherylrmoser@gmail.com>
2022-01-24 10:02:32 -0700

    fixed sign up view

--- theater-app/src/views/public/signup.view.tsx
+++ theater-app/src/views/public/signup.view.tsx
@@ -3,35 +3,26 @@ import { ChangeEvent, useContext, useState } from 'react';
 import { auth } from '../../firebase';
 import firebase from 'firebase';
 import { Header } from '../../Components/header/Header';
-import { useState } from "react";
 import TheaterSignUp from "../../Components/theaterSignUp/TheaterSignUp";
 import UserSignUp from "../../Components/userSignUp/UserSignUp";
 
 
 export default function SignupView() {
-	const [ userSignUp, setUserSignUp ] = useState<boolean>(true)
+	const [userSignUp, setUserSignUp] = useState<boolean>(true)
 
 	function signUpHandler() {
 		setUserSignUp(!userSignUp)
 	}
-	
+
 	return (
 
 		<div className="App">
 			<Header />
-      <div>
-        <button disabled={userSignUp} onClick={signUpHandler}>User Sign Up</button>
-        <button disabled={!userSignUp} onClick={signUpHandler}>Theater Sign Up</button>
-        {userSignUp ? <UserSignUp /> : <TheaterSignUp />}
-		  </div>
-			<form onSubmit={submitHandler}>
-				<input required onChange={emailHandler} value={email} type="email" placeholder='Email' />
-				<input required onChange={passwordHandler} value={password} type="password" placeholder='Password' />
-				<input required onChange={nameHandler} value={name} type="text" placeholder='Name' />
-				<input type="text" onChange={zipCodeHandler} value={zipCode} placeholder='Zip Code'/>
-				<input type="submit" value="Sign Up" />
-			</form>
-
-		
+			<div>
+				<button disabled={userSignUp} onClick={signUpHandler}>User Sign Up</button>
+				<button disabled={!userSignUp} onClick={signUpHandler}>Theater Sign Up</button>
+				{userSignUp ? <UserSignUp /> : <TheaterSignUp />}
+			</div>
+		</div>
 	);
 }
