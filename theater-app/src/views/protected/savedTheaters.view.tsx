b48b11d95d405ed05bc3d3fdd405333a4f8238b6
Stone595 <stone.ruiz2927@stu.mtec.edu>
2022-01-25 09:51:51 -0700

    add base layout for saved Theaters

--- theater-app/src/views/protected/savedTheaters.view.tsx
+++ theater-app/src/views/protected/savedTheaters.view.tsx
@@ -1,7 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import { Header } from "../../Components/header/Header";
+import { AuthContext } from "../../context/AuthContext";
+import { db } from "../../firebase";
+
 export function SavedTheaterView() {
+    // user
+    const user = useContext(AuthContext);
+    const [savedTheaters, setSavedTheaters ] = useState<any>()
+
+    // // grab all of the users saved theaters
+    const userSavedTheaters = async () => {
+        let userCollection =  db.collection('users').doc(user?.uid)
+
+         userCollection.get().then((doc) => {
+            if (doc.exists){
+                const data =  doc.data()
+                setSavedTheaters(data)
+
+            } else {
+                console.log('not found')
+            }
+        }).catch((err) => console.log(err))
+
+    }
+    // userSavedTheaters()
+
+    // useEffect(() => {
+    //   console.log(savedTheaters)
+    // }, []);
+    
+    // console.log(userSavedTheaters())
+    // console.log(user?.uid)
     return (
-        <div>
+        <>
+            <Header />
+
             Saved Theaters
-        </div>
+        </>
     )
 }
