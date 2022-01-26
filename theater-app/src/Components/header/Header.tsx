b672d4b34ec489f2e77ae461e0c59acfb29d0b15
sherylmoser <sherylrmoser@gmail.com>
2022-01-26 09:39:25 -0700

    changed header logo

--- theater-app/src/Components/header/Header.tsx
+++ theater-app/src/Components/header/Header.tsx
@@ -23,37 +23,38 @@ export function Header() {
 
 
     return (
-            <div className="ui secondary pointing menu header">
-                <a href="/" className="item">
-                    <Image src="../logo.png" className="header-logo" />
-                </a>
-                <a href='/performance_search' className="item">
-                    Theater Search
-                </a>
-                <a href="/audtion_search" className="item">
-                    Auditions
-                </a>
-                {
-                    user ? <a href="/saved_theaters" className="item">Saved Theater</a> : null
+        <div className="ui secondary pointing menu header">
+            <a href="/" className="item logo">
+                <Image src="../logo.png" className="header-logo" />
+                <span className="cursive-logo">On Stage</span>
+            </a>
+            <a href='/performance_search' className="item">
+                Theater Search
+            </a>
+            <a href="/audtion_search" className="item">
+                Auditions
+            </a>
+            {
+                user ? <a href="/saved_theaters" className="item">Saved Theater</a> : null
+            }
+
+            <div className="right menu">
+                {user ?
+                    <div className="logged-In-Header">
+                        <span>Welcome, {user?.displayName}</span>
+
+                        <Button onClick={() => { console.log('logged out'); auth.signOut(); window.localStorage.removeItem("TheaterCompany") }}>Sign Out</Button>
+                    </div>
+                    :
+
+
+                    <div className="logged-In-Header">
+                        <Button onClick={() => { nav('/login'); }}>Login</Button>
+                        <Button onClick={() => { nav('/sign_up') }}>Sign up</Button>
+                    </div>
                 }
-                
-                <div className="right menu">
-                    {user ?
-                        <div className="logged-In-Header">
-                            <span>Welcome, {user?.displayName}</span>
-
-                            <Button onClick={() => { console.log('logged out'); auth.signOut(); window.localStorage.removeItem("TheaterCompany") }}>Sign Out</Button>
-                        </div>
-                        :
-
-
-                        <div className="logged-In-Header">
-                            <Button onClick={() => { nav('/login');}}>Login</Button>
-                            <Button onClick={() => { nav('/sign_up')}}>Sign up</Button>
-                        </div>
-                        }
-                </div>
             </div>
-        
+        </div>
+
     )
 }
